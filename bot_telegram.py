b164ba2be400756d194af34b9e84f3154023e421
Efim1232 <104681630+Efim1232@users.noreply.github.com>
2022-06-27 14:47:55 +0300

    Add files via upload

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,13 +1,13 @@
 from aiogram.utils import executor	
 from create_bot import dp
+from handlers import client,admin,other
+from data_base import sqlite_db
 
 async def on_startup(_):
-
 	print('Бот вышел в онлайн')
+	sqlite_db.sql_start()
 
 
-
-from handlers import client,admin,other
 
 client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
